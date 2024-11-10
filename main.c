5539446c99a0c85f179d148806301644330efab1
Sagar Ladla <29002865+sagarladla@users.noreply.github.com>
2024-11-10 17:42:42 +0000

    blink led example

--- main.c
+++ main.c
@@ -11,17 +11,40 @@ __attribute__((constructor)) void before_main(void)
 	while (1);
 }
 
+// led on stm32f103c8t6 is connected to PC13 pin
+void set_led_gpio(void)
+{
+	// GPIOC port clock enable
+	RCC_APB2ENR |= (1 << 4);
+
+	// set up CNF13 bits as general purpose output push pull mode
+	GPIOC_CRH &= ~(1 << 23);
+	GPIOC_CRH &= ~(1 << 22);
+
+	// set up MODE13 bits for max 2 MHz output speed
+	GPIOC_CRH &= ~(1 << 21);
+	GPIOC_CRH |= (1 << 20);
+}
+
+void delay()
+{
+	int i = 30000;
+	int j = 30000;
+	while (i--);
+	while (j--);
+	return;
+}
+
 int main(void)
 {
-	GPIOC_ODR |= (1 << 13);
+	set_led_gpio();
 	__asm__("mov r8, #0x01");
 	while (1)
 	{
-		/*
-		GPIOC_BSRR |= (1 << 13);
-		for (volatile uint32_t i = 0; i < 25000; i++);
-		GPIOC_BSRR &= ~(1 << 13);
-		for (volatile uint32_t i = 0; i < 25000; i++);*/
+		GPIOC_ODR &= ~(1 << 13);
+		delay();
+		GPIOC_ODR |= (1 << 13);
+		delay();
 	}
 	return 0;
 }
